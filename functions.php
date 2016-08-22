ea0b33392b1ce592f3547dd0aa2e90e56b48c8b6
Charles J Dillon <charles.dillon2@wsu.edu>
2016-08-22 11:50:27 -0700

    Adds image tool tips so that desktop users can preview screenshots on how to

--- functions.php
+++ functions.php
@@ -3,5 +3,6 @@
 add_action( 'wp_enqueue_scripts', 'wsu_alert_enqueue_scripts' );
 
 function wsu_alert_enqueue_scripts() {
-	wp_enqueue_script( 'wsu-alert-accordion', get_stylesheet_directory_uri() . '/js/accordion.js', array( 'jquery' ), spine_get_script_version(), true );
+	wp_enqueue_script( 'wsu-alert-functions', get_stylesheet_directory_uri() . '/js/functions.js', array( 'jquery' ), spine_get_script_version(), true );
+	wp_enqueue_script( 'wsu-alert-tooltip', get_stylesheet_directory_uri() . '/js/dw_tooltip_c.js', array( 'jquery' ), spine_get_script_version(), true );
 }
--- js/accordion.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-    $( ".accordion" ).accordion({
-      collapsible: true,
-      active: true,
-        header:"h4",
-      heightStyle: "content"
-    });
-  } );
