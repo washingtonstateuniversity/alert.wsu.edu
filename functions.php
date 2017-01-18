cbad7df43b5d51be6ea1f960e5cc91e0fb7be322
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-18 14:43:57 -0800

    Filter the spine child theme version for cache busting

--- functions.php
+++ functions.php
@@ -6,3 +6,15 @@ function wsu_alert_enqueue_scripts() {
 	wp_enqueue_script( 'wsu-alert-functions', get_stylesheet_directory_uri() . '/js/functions.js', array( 'jquery' ), spine_get_script_version(), true );
 	wp_enqueue_script( 'wsu-alert-tooltip', get_stylesheet_directory_uri() . '/js/dw_tooltip_c.js', array( 'jquery' ), spine_get_script_version(), true );
 }
+
+add_filter( 'spine_child_theme_version', 'wsu_alert_theme_version' );
+/**
+ * Provides an updated version number to help in breaking browser cache.
+ *
+ * @since 0.1.0
+ *
+ * @return string
+ */
+function wsu_alert_theme_version() {
+	return '0.0.7';
+}
