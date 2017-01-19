5952fdc7a38bf7bc1930df762ce5a2d87060bfbb
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-18 17:00:23 -0800

    Break JS cache with spine_get_child_version

--- functions.php
+++ functions.php
@@ -1,9 +1,11 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'wsu_alert_enqueue_scripts' );
-
+/**
+ * Enqueues the scripts used by the child theme.
+ */
 function wsu_alert_enqueue_scripts() {
-	wp_enqueue_script( 'wsu-alert-functions', get_stylesheet_directory_uri() . '/js/functions.js', array( 'jquery' ), spine_get_script_version(), true );
+	wp_enqueue_script( 'wsu-alert-functions', get_stylesheet_directory_uri() . '/js/functions.js', array( 'jquery' ), spine_get_child_version(), true );
 }
 
 add_filter( 'spine_child_theme_version', 'wsu_alert_theme_version' );
